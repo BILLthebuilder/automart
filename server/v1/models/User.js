a77db8574dc4473f149825890484de5d4a55341a
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-05 01:56:03 +0300

    [feature #166397764] modify user model

--- server/v1/models/User.js
+++ server/v1/models/User.js
@@ -3,19 +3,24 @@ class User {
         this.users = [];
     }
 
-    create(data) {
+    create(email, firstName, lastName, password, address, isAdmin) {
         const newUser = {
-            id: data.id,
-            email: data.email,
-            first_name: data.firstName,
-            last_name: data.lastName,
-            password: data.password,
-            address: data.address,
-            isAdmin: data.true
+            id: this.users.length + 1,
+            email,
+            first_name: firstName,
+            last_name: lastName,
+            password,
+            address,
+            isAdmin
         };
         this.users.push(newUser);
         return newUser;
     }
 }
+// const John = new User();
+// John.create('john@gmail.com', 'John', 'Omondi', 'wjnjdasj', '233nairobi', true);
+// John.create('pat@gmail.com', 'patrick', 'Omondi', 'wjnjdasj', '233nairobi', false);
+// John.create('stacy@gmail.com', 'John', 'Omondi', 'wjnjdasj', '233nairobi', true);
+// console.log(John);
 
 export default new User();
