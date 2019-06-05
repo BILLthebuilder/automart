2835fecc1f726ccd8d1fd89e7f54f882dd565398
BILLthebuilder <billkariri@gmail.com>
2019-06-05 20:27:51 +0300

    [feature #166397764] modify app entry point

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 import express from 'express';
 
+import signupRoute from './server/v1/routes/signup';
+
 const app = express();
-const signup = {};
+app.use(signupRoute);
+
+// app.get('/help', (req, res) => {
+//     res.status(201).send({
+//         cow: 'eaten',
+//         goat: 'stolen'
+//     });
+// });
+
 const port = process.env.PORT || 3000;
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server running on port ${port}...`));
 
-export default signup;
+// export default signupRoute;
