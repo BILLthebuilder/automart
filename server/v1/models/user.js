99d172b6fe70b77dddcd1cd03ff8fc1c34cdadd1
BILLthebuilder <billkariri@gmail.com>
2019-06-06 11:49:54 +0300

    [feature #166397764] remove es5 test code

--- server/v1/models/user.js
+++ server/v1/models/user.js
@@ -12,23 +12,3 @@ const users = [
 ];
 
 export default users;
-
-// "use strict";
-
-// Object.defineProperty(exports, "__esModule", {
-//   value: true
-// });
-// exports.default = void 0;
-
-// /* eslint-disable class-methods-use-this */
-// var users = [{
-//   id: 0,
-//   email: '',
-//   first_name: '',
-//   last_name: '',
-//   password: '',
-//   address: '',
-//   isAdmin: true
-// }];
-// var _default = users;
-// exports.default = _default;
