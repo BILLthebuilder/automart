7a64c4b7c0d0da174486090d87f0bfa456c6051a
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-06 10:57:08 +0300

    [feature #166397764] refactor app entry,model,routeand test

--- server/v1/routes/signup.js
+++ server/v1/routes/signup.js
@@ -1,10 +1,33 @@
 import express from 'express';
 import signup from '../controllers/signup';
 
-const signupRoute = express();
+const signupRoute = express.Router();
 signupRoute.use(express.json());
 signupRoute.use(express.urlencoded({ extended: false }));
 
-signupRoute.post('api/v1/auth/signup', signup());
+signupRoute.post('/api/v1/auth/signup', signup.newUser);
 
 export default signupRoute;
+
+// "use strict";
+
+// Object.defineProperty(exports, "__esModule", {
+//   value: true
+// });
+// exports.default = void 0;
+
+// var _express = _interopRequireDefault(require("express"));
+
+// var _signup = _interopRequireDefault(require("../controllers/signup"));
+
+// function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// var signupRoute = _express.default.Router();
+
+// signupRoute.use(_express.default.json());
+// signupRoute.use(_express.default.urlencoded({
+//   extended: false
+// }));
+// signupRoute.post('/api/v1/auth/signup', _signup.default.newUser);
+// var _default = signupRoute;
+// exports.default = _default;
