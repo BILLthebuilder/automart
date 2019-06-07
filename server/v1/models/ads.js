4cc72befac077256915c60b482778165922573f9
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-07 22:31:25 +0300

    [feature #166397876] create model for posting an ad

---
+++ server/v1/models/ads.js
@@ -0,0 +1,24 @@
+import moment from 'moment';
+
+class Ads {
+    constructor() {
+        this.ads = [];
+    }
+
+    create(data) {
+        const newAd = {
+            id: this.ads.length + 1,
+            owner: data.owner || '',
+            email: data.email || '',
+            createdOn: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
+            manufacturer: data.manufacturer || '',
+            model: data.model || '',
+            price: data.price || '',
+            state: data.state || '',
+            status: data.status || ''
+        };
+        this.ads.push(newAd);
+        return newAd;
+    }
+}
+export default new Ads();
