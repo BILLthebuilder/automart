be1f124b518f503e4686ece0b5928f2ee96c0de2
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-08 00:28:28 +0300

    [feature #166397876] refactor app entry point

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import routes from './server/v1/routes/signup';
+import routes from './server/v1/routes/routes';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use(routes);
 
 const port = process.env.PORT || 3000;
 // eslint-disable-next-line no-console
