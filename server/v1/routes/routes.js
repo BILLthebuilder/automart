bed177fb84d4d490043ce9832cd9e1264ae3c97e
BILLthebuilder <billkariri@gmail.com>
2019-06-08 00:27:28 +0300

    [feature #166397876] configure route for ad post

--- server/v1/routes/routes.js
+++ server/v1/routes/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import RegisterUser from '../controllers/signup';
+import AdvertHandler from '../controllers/ads';
 
 const routes = Router();
 
-routes.post('/api/v1/auth/signup', RegisterUser.create);
+routes.post('/api/v1/auth/signup/', RegisterUser.create);
 // routes.post('/api/v1/auth/signin', RegisterUser.create);
+routes.post('/api/v1/car/', AdvertHandler.create);
 
 export default routes;
