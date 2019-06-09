7795a12f367ceb726ab3f42966e037ec82c03e6c
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-09 20:38:12 +0300

    [starts #166398368] create failing tests for purchase order

---
+++ src/server/v1/tests/3-order.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+import chai from 'chai';
+import asserttype from 'chai-asserttype';
+import 'chai/register-expect';
+import chaihttp from 'chai-http';
+import app from '../../../app';
+
+chai.use(chaihttp);
+chai.use(asserttype);
+
+describe('Purchase Order', () => {
+    const order = {
+        Data: {
+            id: 1,
+            carId: 1,
+            createdOn: '01/01/2019',
+            status: 'avalable/sold',
+            price: 4.598,
+            priceOffered: 3.48
+        }
+    };
+    it('A user Should be able to create a purchase order', () => {
+        chai.request(app)
+            .post('/api/v1/car/')
+            .send(order)
+            .end((err, res) => {
+                expect(res.body)
+                    .to.have.a.status(201)
+                    .and.to.be.an('object');
+                expect(res.body.Data).to.have.a.property('id');
+                expect(res.body.Data)
+                    .to.have.a.property('carId')
+                    .and.to.be.a('number');
+                expect(res.body.Data)
+                    .to.have.a.property('createdOn')
+                    .and.to.be.a('string');
+                expect(res.body.Data)
+                    .to.have.a.property('status')
+                    .and.to.be.a('string');
+                expect(res.body.Data)
+                    .to.have.a.property('price')
+                    .and.to.be.a('number');
+                expect(res.body.Data)
+                    .to.have.a.property('priceOffered')
+                    .and.to.be.a('number');
+            });
+    });
+});
