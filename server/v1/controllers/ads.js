f643a2d521b35e35cc029e0d941a090f49ee2d2a
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-09 10:26:53 +0300

    [feature #166398284] refactor delete controller

--- server/v1/controllers/ads.js
+++ server/v1/controllers/ads.js
@@ -24,9 +24,10 @@ const AdvertHandler = {
     },
     deleteSpecific(req, res) {
         const deleted = Ads.deleteSpecificAd(parseInt(req.params.id, 10));
-        return res.status(204).json({
-            status: 204,
-            Data: deleted
+        return res.status(200).json({
+            status: 200,
+            Data: 'Car Ad successfully deleted',
+            deleted
         });
     }
 };
