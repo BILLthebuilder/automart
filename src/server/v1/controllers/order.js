57c4df694029310c5aff67412de4f5a8a625ac83
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-14 10:55:14 +0300

    [bug #166577104] add validations for purchase order

--- src/server/v1/controllers/order.js
+++ src/server/v1/controllers/order.js
@@ -1,7 +1,16 @@
+import Joi from '@hapi/joi';
 import Order from '../models/order';
+import { orderSchema } from '../helpers/validations';
 
 const OrderHandler = {
     create(req, res) {
+        const result = Joi.validate(req.body, orderSchema);
+        if (result.error) {
+            return res.status(400).json({
+                status: 400,
+                error: result.error.details[0].message
+            });
+        }
         const order = Order.create(req.body);
         return res.status(201).json({
             status: 201,
