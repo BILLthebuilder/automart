5e6ff30af3bf93a25f82be3a7405d006f34e7890
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-18 05:48:28 +0300

    [chore #166739053] configure routes for the app

---
+++ src/server/v2/routes/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+// import RegisterUser from '../controllers/userController';
+// import AdvertHandler from '../controllers/ads';
+// import OrderHandler from '../controllers/order';
+
+const routes = Router();
+
+// routes.post('/api/v1/auth/signup/', RegisterUser.create);
+// routes.post('/api/v1/auth/signin', RegisterUser.create);
+// routes.post('/api/v1/car/', AdvertHandler.create);
+// routes.get('/api/v1/cars/', AdvertHandler.getAll);
+// routes.post('/api/v1/order/', OrderHandler.create);
+// routes.get('/api/v1/car/:id', AdvertHandler.viewSpecific);
+// routes.delete('/api/v1/car/:id', AdvertHandler.deleteSpecific);
+
+export default routes;
