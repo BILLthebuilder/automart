0a92fd4e025670b4c743d850392e703506c863d8
BILLthebuilder <billkariri@gmail.com>
2019-06-18 16:28:04 +0300

    [feature #166753623] configure the user controller

---
+++ src/server/v2/controllers/userController.js
@@ -0,0 +1,47 @@
+/* eslint-disable class-methods-use-this */
+import bcrypt from 'bcrypt';
+import Joi from '@hapi/joi';
+// import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import '@babel/polyfill';
+import { userSchema } from '../middlewares/validations';
+import pool from '../config/config';
+
+dotenv.config();
+const user = {
+    async signup(req, res) {
+        const { error } = Joi.validate(req.body, userSchema);
+        if (error) {
+            return res.status(400).json({
+                status: 400,
+                error: error.details[0].message
+            });
+        }
+        const newUser = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: bcrypt.hashSync(req.body.password, 10),
+            email: req.body.email,
+            address: req.body.address,
+            isAdmin: false
+        };
+
+        const insert = `INSERT INTO users (firstName, lastName, password, email, 
+            address, isAdmin) VALUES ($1, $2, $3, $4, $5, $6) returning *`;
+        const results = await pool.query(insert, [
+            newUser.firstName,
+            newUser.lastName,
+            newUser.password,
+            newUser.email,
+            newUser.address,
+            newUser.isAdmin
+        ]);
+        return res.status(201).json({
+            status: 201,
+            message: 'User Created successfully',
+            data: results.rows[1]
+        });
+    }
+};
+
+export default user;
