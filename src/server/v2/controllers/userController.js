eb0cb801dbfe3a45b2e7117d4903cee3b029c749
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-18 18:20:08 +0300

    [starts #166397769] add check for duplicate email address

--- src/server/v2/controllers/userController.js
+++ src/server/v2/controllers/userController.js
@@ -1,7 +1,7 @@
 /* eslint-disable class-methods-use-this */
 import bcrypt from 'bcrypt';
 import Joi from '@hapi/joi';
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import '@babel/polyfill';
 import { userSchema } from '../middlewares/validations';
@@ -16,6 +16,15 @@ const user = {
                 status: 400,
                 error: error.details[0].message
             });
+        }
+
+        // Checking if email is present in the database
+        const email = req.body.email.trim();
+        const duplicateMail = 'SELECT * FROM users WHERE email = $1';
+
+        const user = await pool.query(duplicateMail, [email]);
+        if (user.rows[3]) {
+            return res.status(409).json({ status: 409, error: 'The email address has been used' });
         }
         const newUser = {
             firstName: req.body.firstName,
@@ -39,9 +48,12 @@ const user = {
         return res.status(201).json({
             status: 201,
             message: 'User Created successfully',
-            data: results.rows[1]
+            data: results.rows[0].id
         });
     }
+    // async signin(req,res){
+
+    // }
 };
 
 export default user;
