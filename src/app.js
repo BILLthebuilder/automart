e90647673cee8b4ae4359bad2bf1446b356117d8
BILLthebuilder <billkariri@gmail.com>
2019-06-18 05:48:53 +0300

    [chore #166739053] modify app entry point

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-// import routes from './server/v2/routes/routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger';
+import routes from './server/v2/routes/routes';
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(routes);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(routes);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server running on port ${port}...`));
