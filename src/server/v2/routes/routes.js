9d7fca7359c4f4df0fdf5563c750998f7a221466
BILLthebuilder <billkariri@gmail.com>
2019-06-18 16:28:50 +0300

    [feature #166753623] add signup route

--- src/server/v2/routes/routes.js
+++ src/server/v2/routes/routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-// import RegisterUser from '../controllers/userController';
+import user from '../controllers/userController';
 // import AdvertHandler from '../controllers/ads';
 // import OrderHandler from '../controllers/order';
 
 const routes = Router();
 
-// routes.post('/api/v1/auth/signup/', RegisterUser.create);
+routes.post('/api/v1/auth/signup/', user.signup);
 // routes.post('/api/v1/auth/signin', RegisterUser.create);
 // routes.post('/api/v1/car/', AdvertHandler.create);
 // routes.get('/api/v1/cars/', AdvertHandler.getAll);
