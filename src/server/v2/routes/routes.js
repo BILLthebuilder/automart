ef157b5e4c7d988fd35c5665e505ef5025a68ce1
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-19 13:07:34 +0300

    [finishes #166783728] create route for posting an ad

--- src/server/v2/routes/routes.js
+++ src/server/v2/routes/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import user from '../controllers/userController';
-// import AdvertHandler from '../controllers/ads';
+import ads from '../controllers/adsController';
 // import OrderHandler from '../controllers/order';
 
 const routes = Router();
 
 routes.post('/api/v1/auth/signup/', user.signup);
 routes.post('/api/v1/auth/signin', user.login);
-// routes.post('/api/v1/car/', AdvertHandler.create);
+routes.post('/api/v1/car/', ads.create);
 // routes.get('/api/v1/cars/', AdvertHandler.getAll);
 // routes.post('/api/v1/order/', OrderHandler.create);
 // routes.get('/api/v1/car/:id', AdvertHandler.viewSpecific);
