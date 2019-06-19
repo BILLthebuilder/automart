412649f486c856ac3fca8303a9df2b8dbb9bb5a4
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-20 00:15:13 +0300

    [finishes #166806472] configure route to view specific ad

--- src/server/v2/routes/routes.js
+++ src/server/v2/routes/routes.js
@@ -5,12 +5,12 @@ import orders from '../controllers/orderController';
 
 const routes = Router();
 
-routes.post('/api/v1/auth/signup/', user.signup);
-routes.post('/api/v1/auth/signin', user.login);
-routes.post('/api/v1/car/', ads.create);
-routes.post('/api/v1/order/', orders.create);
-// routes.get('/api/v1/cars/', AdvertHandler.getAll);
-// routes.get('/api/v1/car/:id', AdvertHandler.viewSpecific);
+routes.post('/api/v2/auth/signup/', user.signup);
+routes.post('/api/v2/auth/signin', user.login);
+routes.post('/api/v2/car/', ads.create);
+routes.post('/api/v2/order/', orders.create);
+routes.get('/api/v2/cars/:id', ads.viewSpecific);
+// routes.get('/api/v2/cars/', AdvertHandler.viewAll);
 // routes.delete('/api/v1/car/:id', AdvertHandler.deleteSpecific);
 
 export default routes;
