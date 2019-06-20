d0cbbcbf95e960518bc8a44647a7f72489c97096
BILLthebuilder <billkariri@gmail.com>
2019-06-20 10:36:11 +0300

    [finishes #166398276] create route for viewing unsold price range

--- src/server/v2/routes/routes.js
+++ src/server/v2/routes/routes.js
@@ -4,14 +4,18 @@ import ads from '../controllers/adsController';
 import orders from '../controllers/orderController';
 
 const routes = Router();
-
+// Auth routes
 routes.post('/api/v2/auth/signup/', user.signup);
 routes.post('/api/v2/auth/signin', user.login);
+
+// Car advert routes
 routes.post('/api/v2/car/', ads.create);
-routes.post('/api/v2/order/', orders.create);
 routes.get('/api/v2/cars/:id', ads.viewSpecific);
 routes.get('/api/v2/status/cars', ads.viewUnsold);
 routes.get('/api/v2/cars', ads.viewAll);
 routes.delete('/api/v2/car/:id', ads.deleteSpecific);
+routes.get('/api/v2/range/cars', ads.viewUnsoldPriceRange);
 
+// Purchase order routes
+routes.post('/api/v2/order/', orders.create);
 export default routes;
