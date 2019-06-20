30d8cbec6006d544040b57476b1466de0d5afd54
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-20 12:02:49 +0300

    [finishes #166813835] create tests for purchase order

---
+++ src/server/v2/tests/3-order.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+import chai from 'chai';
+import asserttype from 'chai-asserttype';
+import 'chai/register-expect';
+import chaihttp from 'chai-http';
+import app from '../../../app';
+
+chai.use(chaihttp);
+chai.use(asserttype);
+
+describe('Purchase Order', () => {
+    const order = {
+        Data: {
+            id: 1,
+            carId: 1,
+            createdOn: '01/01/2019',
+            status: 'avalable/sold',
+            price: 4.598,
+            priceOffered: 3.48
+        }
+    };
+    it('A user Should be able to create a purchase order', () => {
+        chai.request(app)
+            .post('/api/v2/order/')
+            .send(order)
+            .end((err, res) => {
+                expect(res.body).to.be.an('object');
+            });
+    });
+});
