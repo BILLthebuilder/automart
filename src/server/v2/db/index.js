7d1c0513a5db5013b3910157d694771472d8b227
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-06-26 00:26:20 +0300

    [bug #166889085]update database connection file

--- src/server/v2/db/index.js
+++ src/server/v2/db/index.js
@@ -1,22 +1,25 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
-
+// node environments;
 dotenv.config();
+let pool = {};
+if (process.env.NODE_ENV === 'development') {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL
+    });
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-});
+    pool.on('connect', () => {});
+} else if (process.env.NODE_ENV === 'test') {
+    pool = new Pool({
+        connectionString: process.env.TEST_DB_URL
+    });
 
-export default {
-    query(text, params) {
-        return new Promise((resolve, reject) => {
-            pool.query(text, params)
-                .then(res => {
-                    resolve(res);
-                })
-                .catch(err => {
-                    reject(err);
-                });
-        });
-    }
-};
+    pool.on('connect', () => {});
+} else if (process.env.NODE_ENV === 'production') {
+    pool = new Pool({
+        connectionString: process.env.PROD_DB_URL
+    });
+
+    pool.on('connect', () => {});
+}
+export default pool;
