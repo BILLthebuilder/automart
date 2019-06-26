887f262649263017b77b56e6ea4092599a93a096
BILLthebuilder <billkariri@gmail.com>
2019-06-27 00:29:40 +0300

    [chore #166398398]create function to handle 404 page

--- src/app.js
+++ src/app.js
@@ -11,8 +11,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(routes);
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use((req, res) => {
+	res.status(404).json({
+		status: 404,
+		error: {
+			message: 'The requested url was not found on this server',
+			solution: 'visit / to view the accepted urls'
+		}
+	});
+});
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running on port ${port}...`));
 
