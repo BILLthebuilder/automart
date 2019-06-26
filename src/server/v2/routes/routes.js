98860fa9c5b2284db9aff5d0818d73ef9bf5f733
BILLthebuilder <billkariri@gmail.com>
2019-06-26 22:17:36 +0300

    [bug #166889085]add jwt authorization

--- src/server/v2/routes/routes.js
+++ src/server/v2/routes/routes.js
@@ -5,22 +5,22 @@ import orders from '../controllers/orderController';
 import Auth from '../middlewares/auth';
 
 const routes = Router();
-// const /*tokenVerify */= Auth.verifyToken;
+const tokenVerify = Auth.verifyToken;
 // Auth routes
 routes.post('/api/v2/auth/signup/', user.signup);
 routes.post('/api/v2/auth/signin', user.login);
 
 // Car advert routes
-routes.post('/api/v2/car/', /* tokenVerify,*/ ads.create);
-routes.get('/api/v2/cars/:id', /* tokenVerify,*/ ads.viewSpecific);
-routes.get('/api/v2/status/cars', /*tokenVerify,*/ ads.viewUnsold);
-routes.get('/api/v2/cars', /*tokenVerify,*/ ads.viewAll);
-routes.delete('/api/v2/car/:id', /*tokenVerify,*/ ads.deleteSpecific);
-routes.get('/api/v2/range/cars', /*tokenVerify,*/ ads.viewUnsoldPriceRange);
-routes.patch('/api/v2/car/:id/status', /*tokenVerify,*/ ads.markSold);
-routes.patch('/api/v2/car/:id/price', /*tokenVerify,*/ ads.updatePrice);
+routes.post('/api/v2/car/', tokenVerify, ads.create);
+routes.get('/api/v2/cars/:id', tokenVerify, ads.viewSpecific);
+routes.get('/api/v2/status/cars', tokenVerify, ads.viewUnsold);
+routes.get('/api/v2/cars', tokenVerify, ads.viewAll);
+routes.delete('/api/v2/car/:id', tokenVerify, ads.deleteSpecific);
+routes.get('/api/v2/range/cars', tokenVerify, ads.viewUnsoldPriceRange);
+routes.patch('/api/v2/car/:id/status', tokenVerify, ads.markSold);
+routes.patch('/api/v2/car/:id/price', tokenVerify, ads.updatePrice);
 
 // Purchase order routes
-routes.post('/api/v2/order/', /*tokenVerify,*/ orders.create);
-routes.patch('/api/v2/order/:id/price', /*tokenVerify,*/ orders.updatePrice);
+routes.post('/api/v2/order/', tokenVerify, orders.create);
+routes.patch('/api/v2/order/:id/price', tokenVerify, orders.updatePrice);
 export default routes;
