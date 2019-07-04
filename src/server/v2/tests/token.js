1b0d0aacd3713798b11fb821ba6742aa0c02e428
BILLthebuilder <billkariri@gmail.com>
2019-07-04 13:27:49 +0300

    [finishes #166981921]modify test token

--- src/server/v2/tests/token.js
+++ src/server/v2/tests/token.js
@@ -1,7 +1,12 @@
-// import jwt from 'jsonwebtoken';
-import Helper from '../helpers/helper';
+import jwt from 'jsonwebtoken';
 
-export const token = Helper.generateToken({ id: 1 });
+export const token = jwt.sign(
+    {
+        id: 1
+    },
+    'seccret',
+    { expiresIn: '1d' }
+);
 
 export const wrongToken =
     'eyJhbiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImlhdCI6MTU2MTc0MDU3MSwiZXhwIjoxNTYxODI2OTcxfQ.vBjfSCFmaGdt2Uv7uSknlUsD_BsWDfEDspLjtN8LxZ4';
