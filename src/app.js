0ccd90f093afe585560608115f39672553b652dd
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-07-08 12:32:42 +0300

    fix module export

--- src/app.js
+++ src/app.js
@@ -14,4 +14,4 @@ app.use('/docs/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running on port ${port}...`));
 
-export default app;
+module.exports = app;
