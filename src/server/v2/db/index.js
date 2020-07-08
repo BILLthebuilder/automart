d80ed49b53864ea929c00bf995385505be88fe4b
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-07-08 12:34:38 +0300

    fix database connection issues

--- src/server/v2/db/index.js
+++ src/server/v2/db/index.js
@@ -4,17 +4,14 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
-const _default = {
-    query(text, params) {
-        return new Promise((resolve, reject) => {
-            pool.query(text, params)
-                .then(res => {
-                    resolve(res);
-                })
-                .catch(err => {
-                    reject(err);
-                });
-        });
-    }
+exports.default = function query(text, params) {
+    return new Promise((resolve, reject) => {
+        pool.query(text, params)
+            .then(res => {
+                resolve(res);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
 };
-exports.default = _default;
--- src/server/v2/db/tables.js
+++ src/server/v2/db/tables.js
@@ -1,85 +1,87 @@
-/* eslint-disable object-shorthand */
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
-(function connect() {
-    pool.on('connect', () => {
-        return console.log('connected to the db');
-    });
-})();
-// module.exports = {
-//     createTables: function() {
-//         const userQuery = `CREATE TABLE IF NOT EXISTS users(
-//     id SERIAL PRIMARY KEY,
-//     firstName VARCHAR(150) NOT NULL,
-//     lastName VARCHAR(150) NOT NULL,
-//     password VARCHAR(150) NOT NULL,
-//     email VARCHAR(128) UNIQUE NOT NULL,
-//     address VARCHAR(150) NOT NULL,
-//     isAdmin BOOL NOT NULL
-//     );
-//     CREATE TABLE IF NOT EXISTS cars(
-//     id SERIAL PRIMARY KEY,
-//     owner INT NOT NULL,
-//     createdOn TIMESTAMP,
-//     state VARCHAR(51) NOT NULL,
-//     status VARCHAR(20) NOT NULL,
-//     price INT NOT NULL,
-//     manufacturer VARCHAR(20) NOT NULL,
-//     model VARCHAR(20) NOT NULL,
-//     bodyType VARCHAR(20) NOT NULL,
-//     description VARCHAR(150) NOT NULL,
-//     picture VARCHAR(150),
-//     FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE
-//     );
-//     CREATE TABLE IF NOT EXISTS orders(
-//     id SERIAL PRIMARY KEY,
-//     carId INT NOT NULL,
-//     createdOn TIMESTAMP,
-//     status VARCHAR(20) NOT NULL,
-//     price INT NOT NULL,
-//     priceOffered INT NOT NULL,
-//     FOREIGN KEY (carId) REFERENCES cars(id) ON DELETE CASCADE
-//     );
-//     CREATE TABLE IF NOT EXISTS flags(
-//     id SERIAL PRIMARY KEY,
-//     carId INT NOT NULL,
-//     createdOn TIMESTAMP NOT NULL,
-//     reason VARCHAR(100) NOT NULL,
-//     description VARCHAR(150) NOT NULL,
-//     FOREIGN KEY (carId) REFERENCES cars(id) ON DELETE CASCADE
-//     )`;
-//         pool.query(userQuery)
-//             .then(() => {
-//                 console.log('all tables created successfully');
-//                 pool.end();
-//             })
-//             .catch(err => {
-//                 console.log(err);
-//                 process.exit(0);
-//             });
-//     },
-//     dropTables: function() {
-//         const dropQuery = `DROP TABLE IF EXISTS users CASCADE;
-//     DROP TABLE IF EXISTS cars CASCADE;
-//     DROP TABLE IF EXISTS orders CASCADE;
-//     DROP TABLE IF EXISTS flags CASCADE;
-//     `;
+pool.on('connect', () => {
+    console.log('connected to the db');
+});
+function createTables() {
+    const userQuery = `CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(150) NOT NULL,
+    lastName VARCHAR(150) NOT NULL,
+    password VARCHAR(150) NOT NULL,
+    email VARCHAR(128) UNIQUE NOT NULL,
+    address VARCHAR(150) NOT NULL,
+    isAdmin BOOL NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS cars(
+    id SERIAL PRIMARY KEY,
+    owner INT NOT NULL,
+    createdOn TIMESTAMP,
+    state VARCHAR(51) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    price INT NOT NULL,
+    manufacturer VARCHAR(20) NOT NULL,
+    model VARCHAR(20) NOT NULL,
+    bodyType VARCHAR(20) NOT NULL,
+    description VARCHAR(150) NOT NULL,
+    picture VARCHAR(150),
+    FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE
+    );
+    CREATE TABLE IF NOT EXISTS orders(
+    id SERIAL PRIMARY KEY,
+    carId INT NOT NULL,
+    createdOn TIMESTAMP,
+    status VARCHAR(20) NOT NULL,
+    price INT NOT NULL,
+    priceOffered INT NOT NULL,
+    FOREIGN KEY (carId) REFERENCES cars(id) ON DELETE CASCADE
+    );
+    CREATE TABLE IF NOT EXISTS flags(
+    id SERIAL PRIMARY KEY,
+    carId INT NOT NULL,
+    createdOn TIMESTAMP NOT NULL,
+    reason VARCHAR(100) NOT NULL,
+    description VARCHAR(150) NOT NULL,
+    FOREIGN KEY (carId) REFERENCES cars(id) ON DELETE CASCADE
+    )`;
+    pool.query(userQuery)
+        .then(() => {
+            console.log('all tables created successfully');
+            pool.end();
+        })
+        .catch(err => {
+            console.log(err);
+            process.exit(0);
+        });
+}
 
-//         pool.query(dropQuery)
-//             .then(() => {
-//                 console.log('all tables dropped successfully');
-//                 pool.end();
-//             })
-//             .catch(err => {
-//                 console.log(err);
-//                 process.exit(0);
-//             });
-//     },
-//     pool
-// };
+function dropTables() {
+    const dropQuery = `DROP TABLE IF EXISTS users CASCADE;
+    DROP TABLE IF EXISTS cars CASCADE;
+    DROP TABLE IF EXISTS orders CASCADE;
+    DROP TABLE IF EXISTS flags CASCADE;
+    `;
 
-// require('make-runnable');
+    pool.query(dropQuery)
+        .then(() => {
+            console.log('all tables dropped successfully');
+            pool.end();
+        })
+        .catch(err => {
+            console.log(err);
+            process.exit(0);
+        });
+}
+
+module.exports = {
+    createTables,
+    dropTables,
+    pool
+};
+
+require('make-runnable');
